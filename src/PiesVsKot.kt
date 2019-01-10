780122f5d62685443d6b9fc7ebd6bbf04c8b1eb0
kalug89 <kaluzny_david@hotmail.com>
2019-01-10 21:15:49 +0100

    Start program piesek vs kot

---
+++ src/PiesVsKot.kt
@@ -0,0 +1,36 @@
+class Piesek(waga: Int, szybkosc: Int, glos: Int, sila: Int) {
+
+    var waga = waga
+    var szybkosc = szybkosc
+    var glos = glos
+    var sila = sila
+}
+
+class Kot(waga: Int, szybkosc: Int, glos: Int, sila: Int) {
+    var waga = waga
+    var szybkosc = szybkosc
+    var glos = glos
+    var sila = sila
+
+
+}
+fun wynik(piesek1: Piesek, kot1: Kot) {
+
+    if (piesek1.waga == kot1.waga) {
+        println("Waga jest taka sama")
+    }
+    if (piesek1.waga > kot1.waga) {
+        print("kot wygral")
+    }
+    if (piesek1.waga < kot1.waga) {
+        print("kot wygral")
+    }
+}
+
+fun main(args: Array<String>) {
+        var azor = Piesek(15,20,30,40)
+        var mruczek = Kot(10,20,30,40)
+
+    wynik(azor,mruczek)
+
+}
